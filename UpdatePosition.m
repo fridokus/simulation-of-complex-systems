c50fa7d580847c4172ce40942b0224b5dd47f9fe
Marie Korneliusson <marieko@o7159-02.o7159.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-23 10:08:32 +0100

    hej

--- UpdatePosition.m
+++ UpdatePosition.m
@@ -1,17 +1,21 @@
-function cars = uppdatePosition(cars,roadDecision,nodes,roads,timeStep)
+function cars = UpdatePosition(cars,roadDecision,nodes,roads,timeStep)
     % Uppdate postion and road parameter 
+    global positionIndex;
+    global currentVelocityIndex;
+    global roadIndex;
+    global nextRoadIndex;
     
-    oldPosition = cars(:,1);
-    velocity = cars(:,4);
-    oldRoad = cars(:,2);
-    newRoad = cars(:,3);
+    
+    oldPosition = cars(:,positionIndex);
+    velocity = cars(:,currentVelocityIndex);
+    oldRoad = cars(:,roadIndex);
+    newRoad = cars(:,nextRoadIndex);
     distVector = nodes(roads(oldRoad,1),:)-nodes(roads(oldRoad,2),:);
     lengthOldRoad = sqrt(distVector(:,1).^2+distVector(:,2).^2); 
 
     newPosition = oldPosition + timeStep*velocity -roadDecision.*lengthOldRoad;
-    cars(:,1) = newPosition;
-    cars(:,2) = newRoad.*roadDecision +(1-roadDecision).*oldRoad;
-    cars = cars;
+    cars(:,positionIndex) = newPosition;
+    cars(:,roadIndex) = newRoad.*roadDecision +(1-roadDecision).*oldRoad;
     
 end
 
