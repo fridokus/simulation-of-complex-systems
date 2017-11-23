7e97b1a62a6d9eb97db1056f0347d2c453df861d
proundeadzx <mateli@student.chalmers.se>
2017-11-23 09:44:32 +0100

    intersection och main

--- traffictest.m
+++ traffictest.m
@@ -5,18 +5,35 @@ clc
 numberOfCars = 1;
 numberOfIterations = 9000;
 
+nodes = [4 1; 4 30; 30 30; 30 1];
+roads = [1 2; 2 3; 3 4; 4 1];
 
-nodes = [0 1; 20 17];
-roads = [1 2];
+cars = [0 1 0.035 0 0.002 0.1 3 2; 6 1 0.03 0 0.002 0.1 3 2; 2 1 0.03 0 0.004 0.1 3 2]
 
-cars = [0 1 0.035 0 3; 6 1 0.03 0 3; 10 1 0.01 0 3];
-cars = sortrows(cars, [2 1], 'descend')
+global positionIndex;
+positionIndex = 1;
+global roadIndex;
+roadIndex = 2;
+global maxVelocityIndex;
+maxVelocityIndex = 3;
+global currentVelocityIndex;
+currentVelocityIndex = 4;
+global maxAccelerationIndex;
+maxAccelerationIndex = 5;
+global maxDeaccelerationIndex;
+maxDeaccelerationIndex = 6;
+global visionIndex;
+visionIndex = 7;
+global nextRoadIndex;
+nextRoadIndex = 8;
+
+cars = sortrows(cars, [2 1], 'descend');
 
 for i = 1:numberOfIterations
-  cars = updateCarPositions(cars, nodes, roads)
-  plotCoordinates = parameterCoordinates(cars, nodes, roads)
+  cars = updateCars(cars, nodes, roads);
+  plotCoordinates = parameterCoordinates(cars, nodes, roads);
 
   scatter(plotCoordinates(:,1), plotCoordinates(:,2))
-  axis([0 25 0 25])
+  axis([0 40 0 40])
   drawnow
 end
