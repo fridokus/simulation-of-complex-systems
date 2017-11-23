dba86e0177e63b36c7d072ab02709c0834815f47
Marie Korneliusson <marieko@o7159-02.o7159.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-23 09:52:45 +0100

    hej

---
+++ UpdatePosition.m
@@ -0,0 +1,19 @@
+function cars = uppdatePosition(cars,roadDecision,nodes,roads,timeStep)
+    % Uppdate postion and road parameter 
+    
+    oldPosition = cars(:,1);
+    velocity = cars(:,4);
+    oldRoad = cars(:,2);
+    newRoad = cars(:,3);
+    distVector = nodes(roads(oldRoad,1),:)-nodes(roads(oldRoad,2),:);
+    lengthOldRoad = sqrt(distVector(:,1).^2+distVector(:,2).^2); 
+
+    newPosition = oldPosition + timeStep*velocity -roadDecision.*lengthOldRoad;
+    cars(:,1) = newPosition;
+    cars(:,2) = newRoad.*roadDecision +(1-roadDecision).*oldRoad;
+    cars = cars;
+    
+end
+
+    
+    
