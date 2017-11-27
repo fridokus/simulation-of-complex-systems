f2e3c541e69653300d84dfc1d20746645634b595
proundeadzx <mateli@student.chalmers.se>
2017-11-27 17:26:57 +0100

    Fixat så att de globala indexvariablerna används i all funktioner

--- parameterCoordinates.m
+++ parameterCoordinates.m
@@ -1,10 +1,10 @@
 function coordinates = parameterCoordinates(cars, nodes, roads)
-  global positionIndex;
-  global roadIndex;
+  global positionIndex
+  global roadIndex
   numberOfNodes = size(nodes,positionIndex);
   startingPoints = [nodes(roads(:,positionIndex)), nodes(roads(:,positionIndex) + numberOfNodes)];
   endPoints = [nodes(roads(:,roadIndex)), nodes(roads(:,roadIndex) + numberOfNodes)];
   directionVectors =  endPoints - startingPoints;
   roadLengths = calculateRoadLength(nodes, roads);
-  coordinates = startingPoints(cars(:,roadIndex),:) + cars(:,roadIndex)./roadLengths(cars(:,roadIndex)) .* directionVectors(cars(:,roadIndex),:);
+  coordinates = startingPoints(cars(:,roadIndex),:) + cars(:,positionIndex)./roadLengths(cars(:,roadIndex)) .* directionVectors(cars(:,roadIndex),:);
 end
