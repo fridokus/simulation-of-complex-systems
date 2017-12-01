0c4e1e22a1958910e4474cbf4838dcf3bbb25ed2
Fannie Henriksson <fannieh@student.chalmers.se>
2017-12-01 09:47:02 +0100

    mer ide till kod

--- PlotMap.m
+++ PlotMap.m
@@ -20,32 +20,21 @@
     % 4. desiredVelocity
     %}
     cars = [0,10,1,20; 0, 20, 2, 20];
-    %        1    2      3        4
-    nodes = [0,0; 0,100; 100,100; 100,110];
+    %        1       2      3        4      5
+    nodes = [50,50; 50,100; 100,100; 100,50;0,50;50,0];
     %roads = [1,2; 2,3; 3,4; 4,1];
-    roads = [1,2;2,3;3,4];
+    roads = [1,2;2,3;3,4;4,1;1,5;5,1;6,1];
     
     nRoads = size(roads,1);
     nCars = size(cars,1);
     
     for iRoad = 1:nRoads
         startNode = roads(iRoad,1);
-        nextStartNode = roads(iRoad+1,1);
-        
         endNode = roads(iRoad,2);
-        nextEndNode = roads(iRoad,2);
-        
         xStart = nodes(startNode,1);
-        nextXStart = nodes(nextStartNode,1);
-        
         yStart = nodes(startNode,2);
-        nextYStart = nodes(nextStartNode,2);
-        
         xEnd = nodes(endNode,1);
-        nextXEnd = nodes(nextEndNode,1);
-        
         yEnd = nodes(endNode,2);
-        nextYEnd = nodes(nextEndNode,2);
         hold on
         %{
         switch neighbours(iRoad)
@@ -64,7 +53,7 @@
                 plot([xStart+1, xEnd-1], [yStart-1, yEnd-1])
         end
         %}
-        if xStart < xEnd 
+        if xStart < xEnd
             plot([xStart-1, xEnd+1], [yStart+1, yEnd+1])
             plot([xStart+1, xEnd-1], [yStart-1, yEnd-1])
         elseif yStart < yEnd
@@ -78,8 +67,10 @@
             plot([xStart-1, xEnd+1], [yStart+1, yEnd+1])
         end
         %}
+        %hold on
+        %plot(roads(iRoad)
         hold on
-        %plot([xStart, xEnd], [yStart, yEnd])
+        plot([xStart, xEnd], [yStart, yEnd])
         
         %plotCoordinates = parameterCoordinates(cars, nodes, roads);
         %scatter(plotCoordinates(:,1), plotCoordinates(:,2))
