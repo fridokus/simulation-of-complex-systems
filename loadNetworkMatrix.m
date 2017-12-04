c4cf1df0416e87f1cf099213046a139845923c21
malindahl <madahl@student.chalmers.se>
2017-12-04 10:35:41 +0100

    GA files

--- loadNetworkMatrix.m
+++
@@ -1,9 +0,0 @@
-function networkMatrix = loadNetworkMatrix
-
-    networkMatrix = [1000 15 10 20 1000;
-                    15  1000 5  1000  7;
-                    10  5 1000  5  8;
-                    20  1000 5  1000  9;
-                    1000   7 8  9  1000];
-                
-end
