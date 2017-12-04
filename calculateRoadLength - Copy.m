c5972fb77c6f36e68caff4c19d8e150043f626be
malindahl <madahl@student.chalmers.se>
2017-12-04 10:41:26 +0100

    removed copied files

--- calculateRoadLength - Copy.m
+++
@@ -1,6 +0,0 @@
-function hypotenuse = calculateRoadLength(nodes, roads)
-  numberOfNodes = size(nodes,1);
-  xLength = nodes(roads(:,1)) - nodes(roads(:,2));
-  ylength = nodes(roads(:,1) + numberOfNodes) - nodes(roads(:,2) + numberOfNodes);
-  hypotenuse = hypot(xLength, ylength);
-end
--- intersection - Copy.m
+++
@@ -1,5 +0,0 @@
-function carsAtIntersection = intersection(cars, roadLengths)
-  global positionIndex;
-  global roadIndex;
-  carsAtIntersection = cars(:,1) >= roadLengths(cars(:,2));
-end
